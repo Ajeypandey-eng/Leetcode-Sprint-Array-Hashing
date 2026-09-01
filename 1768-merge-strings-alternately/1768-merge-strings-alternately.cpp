12b46425a2765a54a985708b045118e6edbb407b
Ajey Pandey <139972828+Ajeypandey-eng@users.noreply.github.com>
2026-09-01 21:25:24 +0530

    Time: 4 ms (32.34%), Space: 8.4 MB (63.21%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0;
+        string final = "";
+        while (i<word1.length() || i<word2.length()){
+            if (i < word1.length()) {
+                final += word1[i];
+            }
+            if (i < word2.length()) {
+                final += word2[i];
+            }
+            i++;
+        }
+    return final;
+    }
+};
